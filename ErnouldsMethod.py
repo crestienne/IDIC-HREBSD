ba3f6782068f617cc951be8d68d978811ac72aae
crestienne <cdechain@andrew.cmu.edu>
2026-02-24 11:27:17 -0500

    spline order was not the problem

--- ErnouldsMethod.py
+++ ErnouldsMethod.py
@@ -168,7 +168,7 @@ def interpolate_pattern(deformed_coords, reference_pattern, PC):
         y_ref,  # y (rows)
         x_ref,  # x (cols)
         reference_pattern,
-        kx=3, ky=3,
+        kx=5, ky=5,
         )
         
     interpolated_pattern = spline(
