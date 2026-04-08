c0161f95aad17c883e9aafa5e62f6affdf86cfff
crestienne <cdechain@andrew.cmu.edu>
2026-04-08 11:25:33 -0400

    Name change just to be clear

--- runner_scripts/runner_Silicon_SimRef.py
+++ runner_scripts/runner_Silicon_SimRef.py
@@ -10,20 +10,21 @@ import utilities
 import get_homography_cpu as core
 import os
 import datetime
+from optimize_reference import optimize_pc_and_euler
 
 
-component = "Si-Indent-With-Mask-SimulatedReference"
-date = "Apr052026" 
+component = "Si-TestRegion-SimulatedReference"
+date = "Apr072026" 
 up2 = (
    '/Users/crestiennedechaine/OriginalData/Si-Indent/001_Si_spherical_indent_20kV.up2' 
 )
 # up2 = "/Users/jameslamb/Documents/research/data/GaN-DED/20240508_27238_512x512_flipX.up2"
-ang = '//Users/crestiennedechaine/OriginalData/Si-Indent/001_Si_spherical_indent_20kV.ang'
+ang = '/Users/crestiennedechaine/OriginalData/Si-Indent/dp2-refined.ang'
 x0 = (0, 0) # reference pattern, order is y,x
 
 #order for roi_slice: [slice(y_start, y_stop), slice(x_start, x_stop)], set to none if want to look at whole pattern 
-#roi_slice= [slice(0, 5), slice(0, 5)]
-roi_slice = None
+roi_slice= [slice(0, 5), slice(0, 10)]
+#roi_slice = None
 
 base_folder_name = f'{component}_{date}_npyfiles'
 foldername = f'/Users/crestiennedechaine/Scripts/DIC-HREBSD/DIC-HREBSD/results/{base_folder_name}/'
@@ -55,6 +56,7 @@ pat_obj.plot_parameter_sweep(
 use_simulated_reference = True
 master_pattern_path = '/Users/crestiennedechaine/OriginalData/Si-Indent/Si-master-20kV.h5'      # e.g. '/path/to/Si-master-20kV.h5'
 tilt_deg = 70.0                 # sample tilt in degrees
+debug_gradients = True        # set True to save debug/gradient_comparison.png
 
 # ------ Run optimization ------
 
@@ -65,7 +67,24 @@ print(x0)
 
 # Read the Euler angles and PC for the reference pattern (used only if use_simulated_reference=True)
 euler_angles_ref = ang_data.eulers[np.unravel_index(x0, ang_data.shape)]  # shape (3,) in radians
+print("Reference pattern Euler angles (radians):", euler_angles_ref)
 pc_ref = ang_data.pc  # (xstar, ystar, zstar)
+print ("Reference pattern PC:", pc_ref)
+
+# ------ Refine PC and Euler angles against the experimental reference ------
+if use_simulated_reference:
+    euler_angles_ref, pc_ref = optimize_pc_and_euler(
+        pat_obj=pat_obj,
+        x0=x0,
+        master_pattern_path=master_pattern_path,
+        euler_angles_init=euler_angles_ref,
+        pc_init=pc_ref,
+        tilt_deg=tilt_deg,
+        max_iter=300,
+        save_dir="debug",
+    )
+    print("Refined Euler angles (rad):", euler_angles_ref)
+    print("Refined PC:", pc_ref)
 
 optimize_params = dict(
     init_type='partial',
@@ -74,12 +93,14 @@ optimize_params = dict(
     n_jobs=8,
     verbose=True,
     roi_slice=roi_slice,
+    scan_shape=ang_data.shape,
     mask=pat_obj.get_mask(),
     use_simulated_reference=use_simulated_reference,
     master_pattern_path=master_pattern_path,
     euler_angles_ref=euler_angles_ref,
     pc_ref=pc_ref,
     tilt_deg=tilt_deg,
+    debug_gradients=debug_gradients,
 )
 
 # ------ Plot real vs simulated reference pattern ------
@@ -92,6 +113,7 @@ if use_simulated_reference:
         PC=pc_ref,
         patshape=pat_obj.patshape,
         tilt_deg=tilt_deg,
+        pat_obj=pat_obj,
     )
     fig, axes = plt.subplots(1, 2, figsize=(10, 5))
     axes[0].imshow(real_pat, cmap="gray")
@@ -108,6 +130,8 @@ if use_simulated_reference:
 h, h_guess, iterations, residuals, dp_norms = core.optimize(
     pat_obj, x0, **optimize_params
 )
+
+
 
 # ------ Save results ------
 
